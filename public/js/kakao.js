54c7c239d59ab094bc458fd24b1c9c3f3a85efa7
juujhooh <pikswn@naver.com>
2024-07-22 00:42:52 +0900

    '카카오이미지재업로드'

--- public/js/kakao.js
+++ public/js/kakao.js
@@ -1,5 +1,5 @@
 // 카카오 SDK 초기화
-Kakao.init(cec305e1f78338a41f1e4fcf5673a4b4); // 발급받은 JavaScript 키로 초기화
+Kakao.init('cec305e1f78338a41f1e4fcf5673a4b4'); // 발급받은 JavaScript 키로 초기화
 
 // 카카오톡 공유하기 버튼 클릭 이벤트
 document.getElementById('kakao-share-btn').addEventListener('click', function() {
@@ -8,7 +8,7 @@ document.getElementById('kakao-share-btn').addEventListener('click', function() 
         content: {
             title: '나경이의 돌잔치 초대장♡',
             description: '나경이의 첫 생일 파티, 여기서 만나요! 🧸',
-            imageUrl: 'https://ifh.cc/g/Hv9fhD.jpg', // 공유할 이미지 URL
+            imageUrl: 'https://ifh.cc/g/q6dSRK.jpg', // 공유할 이미지 URL
             link: {
                 mobileWebUrl: 'https://appropriate-alligator-nagyeong-7fc063a7.koyeb.app',
                 webUrl: 'https://appropriate-alligator-nagyeong-7fc063a7.koyeb.app'
